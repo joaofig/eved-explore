c65426dc184798a8553eb72521416239dd09b726
Joao Paulo Figueira <joao.figueira@gmail.com>
2024-01-14 13:51:25 +0000

    Common speed code

--- common/mapspeed.py
+++ common/mapspeed.py
@@ -4,19 +4,6 @@ import pandas as pd
 from db.api import SpeedDb, EVedDb
 
 
-def get_equivalent_speed(parts: list[tuple[float, float]],
-                         last_speed: float,
-                         seg_len: float) -> float:
-    avg_speed = 0.0
-    dx = 0.0
-    offset = 0.0
-    for offset, speed in parts:
-        avg_speed += (offset - dx) * speed
-        dx = offset
-    avg_speed = (avg_speed + last_speed * (seg_len - offset)) / seg_len
-    return avg_speed
-
-
 def get_all_trips() -> list[(int, int, int)]:
     db = EVedDb()
     sql = "SELECT traj_id, vehicle_id, trip_id FROM trajectory"
@@ -26,24 +13,24 @@ def get_all_trips() -> list[(int, int, int)]:
 def get_edge_speeds(h3_ini: int, h3_end: int, traj_id=-1) -> tuple[float, float, float]:
     db = SpeedDb()
     sql = """
-        SELECT avg_speed from avg_speed 
+        SELECT dt from segment 
         where h3_ini = ? and h3_end = ? and traj_id != ?
     """
     df = db.query_df(sql, (h3_ini, h3_end, traj_id))
     if df.shape[0] > 0:
-        avg_speed = df["avg_speed"].to_numpy()
+        dt = df["dt"].to_numpy()
 
-        median = np.median(avg_speed)
-        min_speed = np.min(avg_speed)
-        std_speed = np.std(avg_speed)
-        avg_speed = np.mean(avg_speed)
+        median = np.median(dt)
+        min_dt = np.min(dt)
+        std_dt = np.std(dt)
+        dt = np.mean(dt)
 
-        if median - std_speed > 0:
-            min_speed = avg_speed - std_speed
+        if median - std_dt > 0:
+            min_dt = dt - std_dt
 
-        return (min_speed,
+        return (min_dt,
                 median,
-                avg_speed + std_speed)
+                dt + std_dt)
     else:
         return 0.0, 0.0, 0.0
 
